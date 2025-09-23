21a5edf9a79d2690e327deb3e82dc4ecd36d5b42
PhucLeDio <dinhphucle3@gmail.com>
2025-09-23 14:11:30 +0700

    feat: adding manifest

---
+++ src/app/head.js
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#ffffff" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" sizes="192x192" href="/icons/icon-192.png" />
+      <link rel="icon" sizes="512x512" href="/icons/icon-512.png" />
+    </>
+  );
+}
